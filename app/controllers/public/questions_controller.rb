af7e57a9c3b50ef1bb6987668df966a0b978f7b0
EC2 Default User <ec2-user@ip-172-31-43-29.eu-north-1.compute.internal>
2025-01-16 03:06:17 +0000

    question 編集

--- app/controllers/public/questions_controller.rb
+++ app/controllers/public/questions_controller.rb
@@ -19,6 +19,33 @@ class Public::QuestionsController < ApplicationController
     @question_answers = @question.question_answers
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question= Question.find(params[:id])
+    if @question.update(question_params)
+      flash[:notice] = '質問を更新しました。'
+      redirect_to questions_path(@question)
+    else
+      flash[:alert] = '質問の更新に失敗しました。'
+      render :edit
+    end
+  end
+
+  def destroy
+    question = Question.find(params[:id])
+    if question.user == current_user # 投稿者本人のみ削除可能
+      question.destroy
+      flash[:notice] = '質問を削除しました。'
+      redirect_to questions_path
+    else
+      flash[:alert] = '権限がありません。'
+      redirect_to questions_path
+    end
+  end
+
   private
 
   def question_params
