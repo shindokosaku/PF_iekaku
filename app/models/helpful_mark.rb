6dfbefcae1d908c9c0c4ab978002fff63779695e
EC2 Default User <ec2-user@ip-172-31-43-29.eu-north-1.compute.internal>
2025-02-19 01:48:03 +0000

    helpful_marks

--- app/models/helpful_mark.rb
+++ app/models/helpful_mark.rb
@@ -3,8 +3,8 @@ class HelpfulMark < ApplicationRecord
   belongs_to :user, optional: true# user_id は NULL を許可（未ログインユーザー対応）
   belongs_to :corporate_user, optional: true
 
-  validates :user_id, uniqueness: { scope: :question_id }, if: :user_id_present?
-  validates :ip_address, uniqueness: { scorpe: :question_id }, if: :ip_address.present?
+  validates :user_id, uniqueness: { scope: :question_id }, if: -> { user_id.present? }
+  validates :ip_address, uniqueness: { scorpe: :question_id }, if: -> { ip_address.present? }
   validates :corporate_user_id, uniqueness: { scope: :question_id }, if: -> { corporate_user_id.present? }
   validate :user_or_ip_must_exist
 
